3daf6fd2ee99a21736245bee20857a1eaa396576
millroy28 <millroy@gmail.com>
2021-12-14 20:47:51 -0500

    CRUD: added autosave.js that will automatically submit the form contents every 60 seconds

---
+++ RhymeBinder/wwwroot/js/autosave.js
@@ -0,0 +1,16 @@
+﻿var autosave_timer;
+autosave_timer = 60; //seconds before autosaving
+
+function autosave_counter() {
+    if (autosave_timer > 0) {
+        autosave_timer--;
+
+        if (autosave_timer > 0) {
+            setTimeout('autosave_counter()', 1000); //elapses one second
+        }
+        if (autosave_timer == 0) {
+            document.getElementById('edit').submit();
+        }
+    }
+}
+autosave_counter();
