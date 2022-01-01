4bb743a43c3e0d82f3e52360870a573189b043fe
millroy28 <millroy@gmail.com>
2022-01-01 15:50:33 -0500

    GUI: added js to populate an area that counts lines as they are entered/edited in text window

--- RhymeBinder/wwwroot/js/editor.js
+++ RhymeBinder/wwwroot/js/editor.js
@@ -164,3 +164,77 @@ var drawer = function () {
 };
 
 drawer();
+
+//Code for LineCounter
+function get_characters_per_line() {
+    var textBox = document.getElementById('body_edit_field');
+    var checkBox = document.getElementById('textWidthSample');
+    var textBoxWidth = textBox.scrollWidth;
+
+    checkBox.hidden = false;
+    var textCharacterWidth = checkBox.clientWidth;
+    checkBox.hidden = true;
+
+    var estCharactersInLine = Math.floor(textBoxWidth / textCharacterWidth);
+    console.log('Estimated Characters per Line: ' + estCharactersInLine);
+    return estCharactersInLine;
+}
+function generate_line_display_string() {
+    var charsInLine = get_characters_per_line();
+    var text = document.getElementById('body_edit_field').value;
+
+    var splitTextArray = text.split("\n");
+    var splitTextArrayCount = splitTextArray.length;
+    var arrayLine;
+    var arrayLineLength;
+    var lineDisplayString = "";
+    var textLineNumber = 1;
+
+    for (let i = 0; i < splitTextArrayCount; i++) {
+        
+        let arrayLine = splitTextArray[i];
+        let arrayLineLength = arrayLine.length;
+        //If arrayline has no characters besides spaces, it is not a line
+        //do with regex at some point?
+
+        console.log("textarea row: " + i + "; row length: " + arrayLineLength + "; max row length: " + charsInLine );
+
+        //if text overruns line, don't count subsequent lines
+        if (arrayLineLength > charsInLine) {
+            console.log('overrun line at ' + i);
+            let overRunFactor = Math.floor(arrayLineLength / charsInLine);
+            lineDisplayString += textLineNumber.toString();
+            textLineNumber++;
+            
+            
+            //add extra line breaks so next line number matches next populated line in text area
+            for (let j = 0; j < overRunFactor; j++) {
+                lineDisplayString += ' \r\n';
+            }
+
+        }
+        if (arrayLineLength > 0 && arrayLineLength <= charsInLine) {
+            console.log('line exists at ' + i);
+            lineDisplayString += textLineNumber.toString();
+            textLineNumber++;
+        }
+
+
+        lineDisplayString += ' \r\n';
+
+    };
+
+    return lineDisplayString;
+}
+function populate_line_count() {
+    let lineCount = generate_line_display_string();
+    document.getElementById('line_count').value = lineCount;
+}
+populate_line_count(); //do it once on load
+
+window.addEventListener("resize", populate_line_count);
+let textboxTyping = document.getElementById('body_edit_field');
+input.addEventListener('keyup', function (e) {
+    populate_line_count();
+});
+
