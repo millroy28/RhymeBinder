6ba73084d28f6a1d2ea847cb7ac0e00164ea93f1
millroy28 <millroy@gmail.com>
2023-01-01 12:09:58 -0500

    Fix locked checkbox in Edit Group Form

--- RhymeBinder/Models/TextGroup.cs
+++ RhymeBinder/Models/TextGroup.cs
@@ -17,9 +17,9 @@ namespace RhymeBinder.Models
         public string GroupTitle { get; set; }
         public int? OwnerId { get; set; }
         public string Notes { get; set; }
-        public bool? Locked { get; set; }
-        public bool? Hidden { get; set; }
-        public int? BinderId { get; set; }
+        public bool Locked { get; set; }
+        public bool Hidden { get; set; }
+        public int BinderId { get; set; }
 
         public virtual Binder Binder { get; set; }
         public virtual SimpleUser Owner { get; set; }
