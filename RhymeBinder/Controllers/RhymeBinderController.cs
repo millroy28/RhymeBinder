3e839d215f16240b2e54f21f298feaca37ec9d47
Roy Miller <61071070+millroy28@users.noreply.github.com>
2023-11-20 22:15:21 -0500

    Clean up unused usings

--- RhymeBinder/Controllers/RhymeBinderController.cs
+++ RhymeBinder/Controllers/RhymeBinderController.cs
@@ -1,11 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using RhymeBinder.Models;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Security.Claims;
-using System.Threading.Tasks;
+
 
 namespace RhymeBinder.Controllers
 {
--- RhymeBinder/Models/ModelHelper.cs
+++ RhymeBinder/Models/ModelHelper.cs
@@ -1,12 +1,6 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using RhymeBinder.Models;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Threading.Tasks;
 
 
 namespace RhymeBinder.Models
