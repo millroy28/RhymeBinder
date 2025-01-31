0cdb4c8dac94081c51851dee66cd7380bc42bc80
Roy Miller <61071070+millroy28@users.noreply.github.com>
2025-01-31 18:21:54 -0500

    Add toggle for list/shelf views

---
+++ RhymeBinder/wwwroot/js/binderView.js
@@ -0,0 +1,15 @@
+﻿
+
+function ShowListView() {
+    document.getElementById("show_shelf_view").textContent = "  Shelf View";
+    document.getElementById("show_list_view").textContent = "►  List View";
+    document.getElementById("shelf_view").hidden = true;
+    document.getElementById("list_view").hidden = false;
+}
+
+function ShowShelfView() {
+    document.getElementById("show_shelf_view").textContent = "► Shelf View";
+    document.getElementById("show_list_view").textContent = "  List View";
+    document.getElementById("shelf_view").hidden = false;
+    document.getElementById("list_view").hidden = true;
+}
