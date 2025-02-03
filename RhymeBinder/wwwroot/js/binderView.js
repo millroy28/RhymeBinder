6ce7e22362b4565ae6d245b5663f36cdc0650dcf
Roy Miller <61071070+millroy28@users.noreply.github.com>
2025-02-02 23:29:39 -0500

    Initial shelf layout

--- RhymeBinder/wwwroot/js/binderView.js
+++ RhymeBinder/wwwroot/js/binderView.js
@@ -3,13 +3,13 @@
 function ShowListView() {
     document.getElementById("show_shelf_view").textContent = "  Shelf View";
     document.getElementById("show_list_view").textContent = "►  List View";
-    document.getElementById("shelf_view").hidden = true;
+    document.getElementById("shelf_view").style.display = "none";
     document.getElementById("list_view").hidden = false;
 }
 
 function ShowShelfView() {
     document.getElementById("show_shelf_view").textContent = "► Shelf View";
     document.getElementById("show_list_view").textContent = "  List View";
-    document.getElementById("shelf_view").hidden = false;
+    document.getElementById("shelf_view").style.display = "grid";
     document.getElementById("list_view").hidden = true;
 }
