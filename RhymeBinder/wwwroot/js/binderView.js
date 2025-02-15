85d7599f565d501f2cf646954918c699b31749fa
Roy Miller <61071070+millroy28@users.noreply.github.com>
2025-02-15 13:33:12 -0500

    Add hover info box to binder shelf view

--- RhymeBinder/wwwroot/js/binderView.js
+++ RhymeBinder/wwwroot/js/binderView.js
@@ -25,3 +25,29 @@ function CloseModal(elementId) {
     document.getElementById(elementId).style.display = "none";
     document.body.style.pointerEvents = 'all';
 }
+
+function SetEventListeners() {
+
+    document.addEventListener('DOMContentLoaded', () => {
+        let binders = document.getElementsByClassName('shelf-view-binder');
+        let hoverCards = document.getElementsByClassName('shelf-view-binder-hover');
+        let hoverTimeout;
+
+        for (let i = 0; i < binders.length; i++) {
+            binders[i].addEventListener('mouseover', (event) => {
+                hoverTimeout = setTimeout(() => {
+                    // Set the hover card position based on mouse coordinates
+                    hoverCards[i].style.left = (event.pageX + 10) + 'px'; // Add 10px offset to the left
+                    hoverCards[i].style.top = (event.pageY + 10) + 'px'; // Add 10px offset to the top
+                    hoverCards[i].style.display = 'block';
+                }, 1000);
+            });
+
+            binders[i].addEventListener('mouseout', () => {
+                clearTimeout(hoverTimeout);
+                hoverCards[i].style.display = 'none';
+            });
+        }
+    });
+
+}
