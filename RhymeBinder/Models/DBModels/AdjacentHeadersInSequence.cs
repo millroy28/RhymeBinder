e3fce4587f9ca023b3f0908205773167e7476cf5
Roy Miller <61071070+millroy28@users.noreply.github.com>
2026-01-01 20:37:10 -0500

    Add modals as checks against misclick

--- RhymeBinder/Models/DBModels/AdjacentHeadersInSequence.cs
+++ RhymeBinder/Models/DBModels/AdjacentHeadersInSequence.cs
@@ -6,6 +6,7 @@
         public int TextHeaderID { get; set; }
         public string GroupTitle { get; set; }
         public int GroupId {  get; set; }
+        public bool IsSequenced { get; set; }
         public int? PreviousInSequenceTextHeaderId { get; set; } = null;
         public string PreviousInSequenceTextHeaderTitle { get; set; } = string.Empty;
         public int? NextInSequenceTextHeaderId { get; set; } = null;
